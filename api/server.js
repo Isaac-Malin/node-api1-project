3201b1a54f42f44bd679a5bdcf2eee1e56802f5a
Isaac Malin <Isaacmalin3530@gmail.com>
2024-02-20 01:03:53 -0700

    completed the get requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,58 @@
-// BUILD YOUR SERVER HERE
+// BUILD YOUR SERVER HERE\
+const express = require("express");
+const User = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+// A GET request to fetch all of the users
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      if (!users) {
+        return res
+          .status(500)
+          .json({ message: "The users information could not be retrieved" });
+      }
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+// A GET request to fetch a single user by ID
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      }
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user" });
+  }
+  User.insert(req.body)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: err.message });
+    });
+});
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const server = require('./api/server');
 
 const port = 9000;
 
+server.listen(port, () => console.log(`server listening on port ${port}`))
+
 // START YOUR SERVER HERE
