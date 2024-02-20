fa6acf621fae4c93f4093607782624f3f65546ed
Isaac Malin <Isaacmalin3530@gmail.com>
2024-02-20 01:38:49 -0700

    the delete requests and the put request are not complete

--- api/server.js
+++ api/server.js
@@ -34,7 +34,7 @@ server.get("/api/users/:id", (req, res) => {
       res.status(200).json(user);
     })
     .catch((err) => {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: "The user information could not be retrieved" });
     });
 });
 
@@ -49,7 +49,43 @@ server.post("/api/users", (req, res) => {
       res.status(201).json(newUser);
     })
     .catch((err) => {
-      res.status(400).json({ message: err.message });
+      res.status(500).json({ message: "There was an error while saving the user to the database" });
+    });
+});
+
+
+server.delete('/api/users/:id', (req, res) => {
+  User.remove(req.params.id)
+    .then(deletedUser => {
+      if(!deletedUser) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist" })
+      } 
+      res.status(200).json(deletedUser)
+    })
+    .catch(err => {
+      res.status(404).json({ message: "The user could not be removed"})
+    })
+})
+
+server.put('/api/users/:id', (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({ message: "Please provide name and bio for the user" });
+  }
+
+  User.update(req.params.id, req.body)
+    .then(updatedUser => {
+      if (!updatedUser) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist" });
+      }
+      // Assuming updatedUser contains the updated fields
+      res.status(200).json({
+        message: "You have successfully updated the user",
+        data: updatedUser
+      });
+    })
+    .catch(err => {
+      console.error("Error updating user:", err);
+      res.status(500).json({ message: "The user information could not be modified" });
     });
 });
 
